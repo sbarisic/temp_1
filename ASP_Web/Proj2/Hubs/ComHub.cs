091199fa6d905480b613930314b0a16c58401d23
Saša Barišić <sasa.barisic@serengetitech.com>
2023-03-13 13:58:03 +0100

    Blazor + SignalR

---
+++ ASP_Web/Proj2/Hubs/ComHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Proj2.Hubs {
+    public class ComHub : Hub {
+        public async Task SendMessage(string user, string message) {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
--- ASP_Web/Proj2/Program.cs
+++ ASP_Web/Proj2/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.ResponseCompression;
 using Proj2.Data;
+using Proj2.Hubs;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +11,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddResponseCompression(opts => {
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
 
 var app = builder.Build();
+
+app.UseResponseCompression();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) {
@@ -25,6 +34,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapBlazorHub();
+app.MapHub<ComHub>("/comhub");
 app.MapFallbackToPage("/_Host");
 
 app.Run();
