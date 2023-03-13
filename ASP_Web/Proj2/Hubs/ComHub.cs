ddf72b0ef80503a2f01aabec0295a702fc247f41
Saša Barišić <sasa.barisic@serengetitech.com>
2023-03-13 14:00:32 +0100

    Debug log

--- ASP_Web/Proj2/Hubs/ComHub.cs
+++ ASP_Web/Proj2/Hubs/ComHub.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics;
 
 namespace Proj2.Hubs {
     public class ComHub : Hub {
         public async Task SendMessage(string user, string message) {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async void DebugLog(string Msg) {
+            Debug.WriteLine("DEBUGLOG: " + Msg);
         }
     }
 }
