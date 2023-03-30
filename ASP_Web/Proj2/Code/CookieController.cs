53b505f8fb5114a1a3f6222f9119ad2eadc4449d
Saša Barišić <sasa.barisic@serengetitech.com>
2023-03-30 15:40:29 +0200

    Pregled uprave, pregled vozila

--- ASP_Web/Proj2/Code/CookieController.cs
+++ ASP_Web/Proj2/Code/CookieController.cs
@@ -9,31 +9,6 @@ namespace Proj2.Code {
 	[ApiController]
 	[RequireHttps]
 	public class CookieController : ControllerBase {
-
-		/*[HttpPost("/deviceaccess")]
-		public JsonResult Post([FromBody] DeviceAccessAPI API) {
-			if (API == null || API.APIVersion == 0)
-				return new JsonResult(new DeviceAccessResponseAPI(DeviceAccessStatus.Invalid));
-
-			if (string.IsNullOrEmpty(API.APIKey))
-				return new JsonResult(new DeviceAccessResponseAPI(DeviceAccessStatus.Forbidden));
-
-			using (DatabaseContext DbCtx = new DatabaseContext()) {
-				DbDeviceAPIKey DbAPIKey = DbCtx.APIKeys.Where(AT => AT.APIKey == API.APIKey).FirstOrDefault();
-
-				if (DbAPIKey == null || DbAPIKey.APIKey != API.APIKey)
-					return new JsonResult(new DeviceAccessResponseAPI(DeviceAccessStatus.Forbidden));
-
-
-				//DbCtx.Database.
-
-				Console.WriteLine("Hello Database World!");
-
-			}
-
-			return new JsonResult(new DeviceAccessResponseAPI(DeviceAccessStatus.OK));
-		}*/
-
 		[HttpPost("/cookie/login")]
 		public async Task<ActionResult> Login([FromForm] string username, [FromForm] string password) {
 			if (AuthStateProvider.Login(username, password, out AuthenticationState AuthState)) {
@@ -42,20 +17,10 @@ namespace Proj2.Code {
 			}
 
 			return Redirect("/login?invalid=1");
-
-			//ClaimsIdentity claimsIdentity = new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.NameIdentifier, name) }, "auth");
-			//ClaimsPrincipal claims = new ClaimsPrincipal(claimsIdentity);
-
-
 		}
 
 		[HttpGet("/cookie/logout")]
 		public async Task<ActionResult> Logout() {
-			//ClaimsIdentity claimsIdentity = new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.NameIdentifier, name) }, "auth");
-			//ClaimsPrincipal claims = new ClaimsPrincipal(claimsIdentity);
-
-			//await HttpContext.SignInAsync(claims);
-
 			await HttpContext.SignOutAsync();
 			return Redirect("/");
 		}
---
+++ ASP_Web/Proj2/Models/PregledUpraveModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Proj2.Models {
+	public class PregledUpraveModel : PageModel {
+
+		public PregledUpraveModel() {
+
+		}
+
+		[BindProperty(SupportsGet = true)]
+		public string Uprava {
+			get; set;
+		}
+	}
+}
