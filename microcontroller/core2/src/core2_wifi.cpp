182a60b11920b10c041f8e820075f71c6655f360
Saša Barišić <sasa.barisic@serengetitech.com>
2023-05-02 18:58:04 +0200

    core2 WiFi

--- microcontroller/core2/src/core2_wifi.cpp
+++ microcontroller/core2/src/core2_wifi.cpp
@@ -1,5 +1,75 @@
 #include <core2.h>
+#include <WiFi.h>
 
-bool core2_wifi_init() {
+void c2_wifi_task(void *params)
+{
+    dprintf("c2_wifi_task() STARTED\n");
+    bool DoNothing = false;
+    bool DoConnect = true;
+
+    for (;;)
+    {
+        wl_status_t WiFiStatus = WiFi.status();
+        dprintf("WiFi.status = %d\n", (int)WiFiStatus);
+
+        switch (WiFiStatus)
+        {
+        case WL_IDLE_STATUS:
+            break;
+
+        case WL_NO_SSID_AVAIL:
+            break;
+
+        case WL_SCAN_COMPLETED:
+            break;
+
+        case WL_CONNECTED:
+            DoNothing = true;
+            break;
+
+        case WL_CONNECT_FAILED:
+            break;
+
+        case WL_CONNECTION_LOST:
+            break;
+
+        case WL_DISCONNECTED:
+            break;
+
+        case WL_NO_SHIELD:
+        default:
+            // TODO: Handle
+            break;
+        }
+
+        if (DoNothing)
+        {
+            DoNothing = false;
+            dprintf("DoNothing\n");
+
+            vTaskDelay(pdMS_TO_TICKS(2000));
+            continue;
+        }
+
+        if (DoConnect)
+        {
+            DoConnect = false;
+            dprintf("DoConnect\n");
+
+            WiFi.mode(WIFI_STA);
+            WiFi.begin("TEST", "123456789");
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+bool core2_wifi_init()
+{
+    dprintf("core2_wifi_init() BEGIN\n");
+
+    xTaskCreate(c2_wifi_task, "c2_wifi_task", 4096, NULL, 1, NULL);
+
+    dprintf("core2_wifi_init() END\n");
     return true;
 }
--- microcontroller/core2/src/main.cpp
+++ microcontroller/core2/src/main.cpp
@@ -34,20 +34,10 @@ void setup()
   core2_oled_init();
   core2_wifi_init();
 
-  lock = core2_lock_create();
-  
-  core2_oled_print("Hello 1");
-  core2_oled_print("Hello 2");
-  core2_oled_print("Hello 3");
-  core2_oled_print("Hello 4");
-  core2_oled_print("Hello 5");
-  core2_oled_print("Hello 6");
-  core2_oled_print("Hello 7");
-  core2_oled_print("Hello 8");
-  core2_oled_print("Hello 9");
+  /*lock = core2_lock_create();
 
   xTaskCreate(test, "test", 4096, NULL, 1, NULL);
-  xTaskCreate(test2, "test2", 4096, NULL, 1, NULL);
+  xTaskCreate(test2, "test2", 4096, NULL, 1, NULL);*/
 }
 
 void loop()
