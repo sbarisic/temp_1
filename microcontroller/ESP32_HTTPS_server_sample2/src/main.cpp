818557267138485aa3919fdfa0784b0331fccf6d
Goran Vidakovic <goran.vidakovic.bj@gmail.com>
2023-06-26 19:43:50 +0200

    Latest

--- microcontroller/ESP32_HTTPS_server_sample2/src/main.cpp
+++ microcontroller/ESP32_HTTPS_server_sample2/src/main.cpp
@@ -15,10 +15,8 @@ const char *password = "OPTIMA2701002212";
 const char *host = "demo.sbarisic.com";
 const int httpsPort = 443;
 
-
-#define PIN_SWITCH (gpio_num_t)15  // !!!
+#define PIN_SWITCH (gpio_num_t)15 // !!!
 QueueHandle_t interruptQueue;
-
 
 // define for MCP3201
 // #define SPI_CS      4        // SPI slave select
@@ -57,50 +55,70 @@ const int gotTempPress = BIT1;
 const int gotLCD = BIT2;
 const int gotVoltageDrop = BIT3;
 
-static void gpio_isr_handler(void *args)  // definicija ISR
+static void gpio_isr_handler(void *args) // definicija ISR
 {
   int pinNumber = (int)args;
-  xQueueSendFromISR(interruptQueue, &pinNumber,NULL);
-
+  xQueueSendFromISR(interruptQueue, &pinNumber, NULL);
 }
 
-void buttonPushedTask (void *params)   // task koji se pokreće nakon aktivacije ISR
+float naponi[200]; // marica za zapis padova napona u interruptu
+
+void buttonPushedTask(void *params) // task koji se pokreće nakon aktivacije ISR
 {
-  int pinNumber,count = 0;
-  while(true)
+  int pinNumber, count = 0;
+  while (true)
   {
-    if(xQueueReceive(interruptQueue, &pinNumber,portMAX_DELAY))
+    if (xQueueReceive(interruptQueue, &pinNumber, portMAX_DELAY))
     {
       gpio_num_t gpinNumber = (gpio_num_t)pinNumber;
 
-      //disable the interrupt
+      // disable the interrupt
       gpio_isr_handler_remove(gpinNumber);
 
-      //wait some time while we check for the pin to be released
+      // wait some time while we check for the pin to be released
       do
       {
         vTaskDelay(5 / portTICK_PERIOD_MS);
       } while (gpio_get_level(gpinNumber) == (gpio_num_t)0);
 
-
-
-      printf ("GPIO %d was pressed %d times. The state is %d \n", pinNumber,count++,gpio_get_level(gpinNumber));
+      printf("GPIO %d was pressed %d times. The state is %d \n", pinNumber, count++, gpio_get_level(gpinNumber));
 
       // TODO: spremiti u memoriju
-       digitalWrite(14,HIGH);
-               
-       delay(3000); // vTaskDelay(3000 / portTICK_PERIOD_MS);
+      // digitalWrite(14, HIGH);
+      // delay(3000); // vTaskDelay(3000 / portTICK_PERIOD_MS);
+      // digitalWrite(14, LOW);
 
-       digitalWrite(14,LOW);
+      for (int i = 0; i < 200; i++)
+      {
+        float minnapon = 35.0;
+        for (int j = 0; j < 20; j++)
+        {
+          uint16_t raw1 = adc1.read(MCP3201::Channel::SINGLE_0);
+          uint16_t val1 = adc1.toAnalog(raw1);
+          float naponj = val1 * 9.215 / 1000;
+          delay(1);
+          if (naponj < minnapon)
+            minnapon = naponj;
+        }
+        naponi[i] = int(minnapon * 100);
+      }
 
-       gpio_isr_handler_add(gpinNumber,gpio_isr_handler,(void *)pinNumber);
+      // vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-       xEventGroupSetBits(eventGroup, gotVoltageDrop); // javljamo da su padovi napona očitani
+      for (int i = 0; i < 200; i++)
+      {
+        // printf(" %f , ", naponi[i]);
+        Serial.print(int(naponi[i]));
+        Serial.print(",");
+      }
+      printf("\n");
 
+      digitalWrite(13, LOW);
+
+      xEventGroupSetBits(eventGroup, gotVoltageDrop); // javljamo da su padovi napona očitani
+      gpio_isr_handler_add(gpinNumber, gpio_isr_handler, (void *)pinNumber);
     }
-
   }
-
 }
 
 void task1(void *parameters)
@@ -200,7 +218,7 @@ void task4(void *pvparameter)
   {
     printf("Task4\n");
 
-    xEventGroupWaitBits(eventGroup, gotVoltage | gotTempPress | gotLCD , true, true, portMAX_DELAY);
+    xEventGroupWaitBits(eventGroup, gotVoltage | gotTempPress | gotLCD, true, true, portMAX_DELAY);
 
     DynamicJsonDocument doc(512);
 
@@ -222,13 +240,14 @@ void task4(void *pvparameter)
       Serial.println("Connection Failed");
       return;
     }
-    
-    uint8_t valid_fingerprint[32] = {0x90, 0xC4, 0x9F, 0xE3, 0xD4, 0x82, 0x78, 0xEE, 0x7E, 0x3, 0xD7, 0xE9, 0x1, 0x2C, 0x2D, 0x10, 0xCF, 0x8D, 0x27, 0x88, 0xF0, 0x20, 0x31, 0x4D, 0xF9, 0x51, 0xF8, 0xF8, 0xA4, 0x15, 0xBF, 0xC7 };
+
+    uint8_t valid_fingerprint[32] = {0x90, 0xC4, 0x9F, 0xE3, 0xD4, 0x82, 0x78, 0xEE, 0x7E, 0x3, 0xD7, 0xE9, 0x1, 0x2C, 0x2D, 0x10, 0xCF, 0x8D, 0x27, 0x88, 0xF0, 0x20, 0x31, 0x4D, 0xF9, 0x51, 0xF8, 0xF8, 0xA4, 0x15, 0xBF, 0xC7};
     uint8_t fingerprint[32];
     client.getFingerprintSHA256(fingerprint);
     Serial.print("Fingerprint: ");
 
-    for (int i = 0; i < 32; i++) {
+    for (int i = 0; i < 32; i++)
+    {
       Serial.print("0x");
       Serial.print(fingerprint[i], HEX);
       Serial.print(", ");
@@ -236,20 +255,25 @@ void task4(void *pvparameter)
     Serial.println();
 
     bool certificate_valid = true;
-    for (int i = 0; i < 32; i++) {
-      if (valid_fingerprint[i] != fingerprint[i]) {
+    for (int i = 0; i < 32; i++)
+    {
+      if (valid_fingerprint[i] != fingerprint[i])
+      {
         certificate_valid = false;
         break;
       }
     }
 
-    if (certificate_valid) {
+    if (certificate_valid)
+    {
       Serial.println("Fingerprint valid");
-    } else {
-      Serial.println("Fingerprint invalid, testing");
-      //return;
     }
-    
+    else
+    {
+      Serial.println("Fingerprint invalid, testing");
+      // return;
+    }
+
     digitalWrite(13, HIGH);
     String url = "/deviceaccess"; // "/deviceaccess_dev";
 
@@ -298,21 +322,20 @@ void task4(void *pvparameter)
 
 void setup()
 {
- // definicija GPIO kao ISR
+  // definicija GPIO kao ISR
   gpio_set_direction(PIN_SWITCH, GPIO_MODE_INPUT);
   gpio_pulldown_en(PIN_SWITCH);
   gpio_pullup_dis(PIN_SWITCH);
-  gpio_set_intr_type(PIN_SWITCH,GPIO_INTR_NEGEDGE);
+  gpio_set_intr_type(PIN_SWITCH, GPIO_INTR_NEGEDGE);
 
-  pinMode(13,OUTPUT);
+  pinMode(13, OUTPUT);
 
-  interruptQueue = xQueueCreate(1,sizeof(int));  // postavljen queue za interrupt
-  xTaskCreate(buttonPushedTask,"button pushed",2048,NULL,1,NULL);  // task koji se izvršava po interruptu 
+  interruptQueue = xQueueCreate(1, sizeof(int));                       // postavljen queue za interrupt
+  xTaskCreate(buttonPushedTask, "button pushed", 2048, NULL, 1, NULL); // task koji se izvršava po interruptu
 
-  gpio_install_isr_service(0);  // instalacija ISR-a
-  gpio_isr_handler_add(PIN_SWITCH,gpio_isr_handler,(void *)PIN_SWITCH);  // dodjela ISR
- 
-  
+  gpio_install_isr_service(0);                                            // instalacija ISR-a
+  gpio_isr_handler_add(PIN_SWITCH, gpio_isr_handler, (void *)PIN_SWITCH); // dodjela ISR
+
   Serial.begin(115200);
 
   eventGroup = xEventGroupCreate();
@@ -401,12 +424,10 @@ void setup()
       NULL, // task handle
       0     // core 1
   );
-   
 }
-  
+
 void loop()
 {
   // if( count1 > 3 && task1_handler != NULL)
   // vTaskSuspend(task1_handler);
 }
-
--- microcontroller/ESP32_RTOS_interrupt_queue/src/main.cpp
+++ microcontroller/ESP32_RTOS_interrupt_queue/src/main.cpp
@@ -3,7 +3,7 @@
 #include <Mcp320x.h>
 #include <SPI.h>
 
-#define PIN_SWITCH (gpio_num_t)15  // !!!
+#define PIN_SWITCH (gpio_num_t)15 // !!!
 
 QueueHandle_t interruptQueue;
 
@@ -14,113 +14,109 @@ MCP3201 adc(ADC_VREF, 4);
 MCP3201 adc1(ADC_VREF, 5);
 
 float voltage_drops;
-float naponi[200];  // marica za zapis padova napona u interruptu
+float naponi[200]; // marica za zapis padova napona u interruptu
 
-static void IRAM_ATTR gpio_isr_handler(void *args)  // definicija ISR
+static void IRAM_ATTR gpio_isr_handler(void *args) // definicija ISR
 {
   int pinNumber = (int)args;
-  xQueueSendFromISR(interruptQueue, &pinNumber,NULL);
-
+  xQueueSendFromISR(interruptQueue, &pinNumber, NULL);
 }
 
-void buttonPushedTask (void *params)   // task koji se pokreće nakon aktivacije ISR
+void buttonPushedTask(void *params) // task koji se pokreće nakon aktivacije ISR
 {
-  int pinNumber,count = 0;
-  while(true)
+  int pinNumber, count = 0;
+  while (true)
   {
-    if(xQueueReceive(interruptQueue, &pinNumber,portMAX_DELAY))
+    if (xQueueReceive(interruptQueue, &pinNumber, portMAX_DELAY))
     {
       gpio_num_t gpinNumber = (gpio_num_t)pinNumber;
 
-      //disable the interrupt
+      // disable the interrupt
       gpio_isr_handler_remove(gpinNumber);
 
-      //wait some time while we check for the pin to be released
+      // wait some time while we check for the pin to be released
       do
       {
         vTaskDelay(5 / portTICK_PERIOD_MS);
       } while (gpio_get_level(gpinNumber) == (gpio_num_t)0);
 
+      printf("GPIO %d was pressed %d times. The state is %d \n", pinNumber, count++, gpio_get_level(gpinNumber));
 
+      digitalWrite(13, HIGH);
 
-      printf ("GPIO %d was pressed %d times. The state is %d \n", pinNumber,count++,gpio_get_level(gpinNumber));
-
-       digitalWrite(13,HIGH);
-
-       for(int i = 0; i < 200; i++){
+      for (int i = 0; i < 200; i++)
+      {
         float minnapon = 35.0;
-          for (int j = 0; j<20;j++){
-            uint16_t raw1 = adc1.read(MCP3201::Channel::SINGLE_0); 
-            uint16_t val1 = adc1.toAnalog(raw1);
-            float naponj = val1 * 9.215 / 1000 ;
-            delay(1);
-            if (naponj < minnapon) minnapon = naponj;
-          }
-          naponi[i] =int (minnapon*100);
-       }
-        
-       // vTaskDelay(1000 / portTICK_PERIOD_MS);
+        for (int j = 0; j < 20; j++)
+        {
+          uint16_t raw1 = adc1.read(MCP3201::Channel::SINGLE_0);
+          uint16_t val1 = adc1.toAnalog(raw1);
+          float naponj = val1 * 9.215 / 1000;
+          delay(1);
+          if (naponj < minnapon)
+            minnapon = naponj;
+        }
+        naponi[i] = int(minnapon * 100);
+      }
 
-        for (int i = 0 ; i < 200; i++){
+      // vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+      for (int i = 0; i < 200; i++)
+      {
         // printf(" %f , ", naponi[i]);
         Serial.print(int(naponi[i]));
         Serial.print(",");
-         }
-        printf("\n");
-      
-       digitalWrite(13,LOW);
+      }
+      printf("\n");
 
-      gpio_isr_handler_add(gpinNumber,gpio_isr_handler,(void *)pinNumber);
+      digitalWrite(13, LOW);
+
+      gpio_isr_handler_add(gpinNumber, gpio_isr_handler, (void *)pinNumber);
     }
-
   }
-
 }
 
-void setup() {
-   // configure PIN mode
+void setup()
+{
+  // configure PIN mode
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
-  pinMode(13,OUTPUT);
+  pinMode(13, OUTPUT);
 
   // set initial PIN state
   digitalWrite(4, HIGH);
   digitalWrite(5, HIGH);
 
-
-   // initialize SPI interface for MCP3208 & LCD
+  // initialize SPI interface for MCP3208 & LCD
   SPISettings settings(ADC_CLK, MSBFIRST, SPI_MODE0);
   SPI.begin();
   SPI.beginTransaction(settings);
-
-   
 
   Serial.begin(115200);
   // definicija GPIO kao ISR
   gpio_set_direction(PIN_SWITCH, GPIO_MODE_INPUT);
   gpio_pulldown_en(PIN_SWITCH);
   gpio_pullup_dis(PIN_SWITCH);
-  gpio_set_intr_type(PIN_SWITCH, GPIO_INTR_HIGH_LEVEL);      //GPIO_INTR_NEGEDGE);
+  gpio_set_intr_type(PIN_SWITCH, GPIO_INTR_HIGH_LEVEL); // GPIO_INTR_NEGEDGE);
 
- 
+  interruptQueue = xQueueCreate(1, sizeof(int));                       // postavljen queue za interrupt
+  xTaskCreate(buttonPushedTask, "button pushed", 2048, NULL, 1, NULL); // task koji se izvršava po interruptu
 
-  interruptQueue = xQueueCreate(1,sizeof(int));  // postavljen queue za interrupt
-  xTaskCreate(buttonPushedTask,"button pushed",2048,NULL,1,NULL);  // task koji se izvršava po interruptu 
-
-  gpio_install_isr_service(0);  // instalacija ISR-a
-  gpio_isr_handler_add(PIN_SWITCH,gpio_isr_handler,(void *)PIN_SWITCH);  // dodjela ISR
+  gpio_install_isr_service(0);                                            // instalacija ISR-a
+  gpio_isr_handler_add(PIN_SWITCH, gpio_isr_handler, (void *)PIN_SWITCH); // dodjela ISR
 }
 
-void loop() {
-          //uint16_t raw = adc.read(MCP3201::Channel::SINGLE_0);
-          uint16_t raw1 = adc1.read(MCP3201::Channel::SINGLE_0); 
+void loop()
+{
+  // uint16_t raw = adc.read(MCP3201::Channel::SINGLE_0);
+  uint16_t raw1 = adc1.read(MCP3201::Channel::SINGLE_0);
 
-          //uint16_t val = adc.toAnalog(raw);
-          uint16_t val1 = adc1.toAnalog(raw1);
+  // uint16_t val = adc.toAnalog(raw);
+  uint16_t val1 = adc1.toAnalog(raw1);
 
-          voltage_drops = val1 * 9.215 / 1000 ;
-        //float voltage_drops1 = val1 * 9.215 / 1000 ;
-          
-         printf("Voltage drops= %.2f \n ", voltage_drops); //,voltage_drops1);
-         vTaskDelay(5000 / portTICK_PERIOD_MS);
+  voltage_drops = val1 * 9.215 / 1000;
+  // float voltage_drops1 = val1 * 9.215 / 1000 ;
+
+  printf("Voltage drops= %.2f \n ", voltage_drops); //,voltage_drops1);
+  vTaskDelay(5000 / portTICK_PERIOD_MS);
 }
