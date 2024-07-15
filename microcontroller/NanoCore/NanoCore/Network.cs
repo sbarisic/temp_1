cfd96187f9676a4b2b878cb94629cf5f60630b9d
Saša Barišić <sbarisic@vub.hr>
2024-07-15 20:21:01 +0200

    nanoframework test

---
+++ microcontroller/NanoCore/NanoCore/Network.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Device.Wifi;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+
+namespace NanoCore {
+	static class Network {
+		static string SoftApIP = "192.168.42.69";
+		static string SoftApSSID = "NanoFramework";
+		static string SoftApPass = "12345678";
+
+		static bool Setup() {
+			NetworkInterface NI = GetInterface();
+			WirelessAPConfiguration WapConf = GetConfiguration();
+
+			if (WapConf.Options == (WirelessAPConfiguration.ConfigurationOptions.Enable | WirelessAPConfiguration.ConfigurationOptions.AutoStart) && NI.IPv4Address == SoftApIP) {
+				return true;
+			}
+
+			NI.EnableStaticIPv4(SoftApIP, "255.255.255.0", SoftApIP);
+
+			WapConf.Options = WirelessAPConfiguration.ConfigurationOptions.AutoStart | WirelessAPConfiguration.ConfigurationOptions.Enable;
+			WapConf.Ssid = SoftApSSID;
+			WapConf.MaxConnections = 1;
+			WapConf.Authentication = AuthenticationType.WPA2;
+			WapConf.Password = SoftApPass;
+			WapConf.SaveConfiguration();
+
+			return false;
+		}
+
+		public static NetworkInterface GetInterface() {
+			NetworkInterface[] Interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+			// Find WirelessAP interface
+			foreach (NetworkInterface ni in Interfaces) {
+				if (ni.NetworkInterfaceType == NetworkInterfaceType.WirelessAP) {
+					return ni;
+				}
+			}
+
+			return null;
+		}
+
+		public static WifiAdapter GetAdapter() {
+			WifiAdapter[] Adapters = WifiAdapter.FindAllAdapters();
+			WifiAdapter Ad = Adapters[0];
+			return Ad;
+		}
+
+		public static WirelessAPConfiguration GetConfiguration() {
+			NetworkInterface ni = GetInterface();
+			return WirelessAPConfiguration.GetAllWirelessAPConfigurations()[ni.SpecificConfigId];
+		}
+
+		public static void Connect(string SSID, string Pass) {
+			WifiAdapter Ad = GetAdapter();
+
+			WifiConnectionResult Con = Ad.Connect(SSID, WifiReconnectionKind.Automatic, Pass);
+
+			if (Con.ConnectionStatus == WifiConnectionStatus.Success) {
+				Debug.WriteLine("Connected!");
+				return;
+			}
+
+			Debug.WriteLine("Status: " + Con.ConnectionStatus.ToString());
+			return;
+
+
+			/*Ad.AvailableNetworksChanged += (Sender, E) => {
+				Debug.WriteLine("AvailableNetworksChanged");
+
+				foreach (WifiAvailableNetwork Net in Sender.NetworkReport.AvailableNetworks) {
+					Debug.WriteLine($"Net SSID :{ Net.Ssid },  BSSID : { Net.Bsid },  rssi : { Net.NetworkRssiInDecibelMilliwatts },  signal : { Net.SignalBars }");
+				}
+
+			};
+
+			while (true) {
+				Ad.ScanAsync();
+				Thread.Sleep(10000);
+			}*/
+		}
+	}
+}
---
+++ microcontroller/NanoCore/NanoCore/Program.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using nanoFramework.Hardware.Esp32;
+using nanoFramework.System.IO.FileSystem;
+using System.Device.Gpio;
+using Iot.Device.Ws28xx;
+using Iot.Device.Ws28xx.Esp32;
+using System.Device.Wifi;
+using System.Drawing;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Net;
+using Iot.Device.DhcpServer;
+using nanoFramework.Networking;
+using nanoFramework.Runtime.Native;
+using Windows.Storage.Devices;
+using System.Device.Spi;
+using SDCard = nanoFramework.System.IO.FileSystem.SDCard;
+using Windows.Storage;
+
+
+namespace NanoCore {
+	public class Program {
+		static GpioController GPIO;
+
+		static void SetupPins_LILYGO_CAN() {
+			GPIO = new GpioController();
+
+			GpioPin Pin5VEN = GPIO.OpenPin(16, PinMode.Output);
+			Pin5VEN.Write(PinValue.High);
+
+			GpioPin PinSDCS = GPIO.OpenPin(13, PinMode.Output);
+			PinSDCS.Write(PinValue.Low);
+
+			Configuration.SetPinFunction(14, DeviceFunction.SPI1_CLOCK);
+			Configuration.SetPinFunction(15, DeviceFunction.SPI1_MOSI);
+			Configuration.SetPinFunction(02, DeviceFunction.SPI1_MISO);
+		}
+
+		public static void Main() {
+			SetupPins_LILYGO_CAN();
+			Debug.WriteLine("Setting up FS");
+
+			StorageFolder[] Folders = KnownFolders.RemovableDevices.GetFolders();
+
+			foreach (StorageFolder F1 in Folders) {
+				PrintFolder(F1, 0);
+			}
+
+
+			File.WriteAllText("D:\\test_dotnet.txt", "Hello World from .NET from ESP32!");
+
+			BlinkLEDs();
+		}
+
+		static void PrintFolder(StorageFolder F1, int Level) {
+			string Prefix = new string('-', Level * 2);
+			Debug.WriteLine(Prefix + F1.Name);
+
+			StorageFolder[] SDFolders = F1.GetFolders();
+			StorageFile[] SDFiles = F1.GetFiles();
+
+			foreach (StorageFolder SDFolder in SDFolders) {
+				PrintFolder(SDFolder, Level + 1);
+			}
+
+			foreach (StorageFile SDFile in SDFiles) {
+				Debug.WriteLine(Prefix + SDFile.Path);
+			}
+		}
+
+
+		static void BlinkLEDs() {
+			Debug.WriteLine("Hello from nanoFramework! Testing LED!");
+
+			Ws28xx Neo = new Ws2812c(4, 1);
+			while (true) {
+				Neo.Image.SetPixel(0, 0, Color.FromArgb(50, 0, 0));
+				Neo.Update();
+
+				Thread.Sleep(500);
+
+				Neo.Image.SetPixel(0, 0, Color.FromArgb(0, 50, 0));
+				Neo.Update();
+
+				Thread.Sleep(500);
+
+				Neo.Image.SetPixel(0, 0, Color.FromArgb(0, 0, 50));
+				Neo.Update();
+
+				Thread.Sleep(500);
+			}
+		}
+	}
+}
